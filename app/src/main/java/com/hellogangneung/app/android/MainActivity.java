160fe89a109862c9535d345849d34b752f99b553
Hassan Abid <hassanabidpk89@gmail.com>
2018-02-07 18:09:17 +0900

    add app center integeration

--- app/src/main/java/com/hellogangneung/app/android/MainActivity.java
+++ app/src/main/java/com/hellogangneung/app/android/MainActivity.java
@@ -3,11 +3,23 @@ package com.hellogangneung.app.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupAppCenter();
+    }
+
+    private void setupAppCenter() {
+
+        AppCenter.start(getApplication(), "",
+                Analytics.class, Crashes.class);
     }
 }
